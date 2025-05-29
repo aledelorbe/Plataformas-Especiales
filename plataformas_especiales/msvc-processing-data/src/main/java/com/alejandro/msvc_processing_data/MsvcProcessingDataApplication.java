af9ae55ed1f0c090d97c33f4001d5c9d63a35334
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2025-05-29 12:46:58 -0600

    create a modular project and first msvc

---
+++ plataformas_especiales/msvc-processing-data/src/main/java/com/alejandro/msvc_processing_data/MsvcProcessingDataApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.msvc_processing_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcProcessingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcProcessingDataApplication.class, args);
+	}
+
+}
---
+++ plataformas_especiales/msvc-processing-data/src/test/java/com/alejandro/msvc_processing_data/MsvcProcessingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.msvc_processing_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProcessingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
